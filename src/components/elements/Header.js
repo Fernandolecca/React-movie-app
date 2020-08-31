1da0f784ce77638e00cd133ad03016fde072da53
Fernando lecca <fernandolecca93@gmail.com>
2020-08-31 12:33:58 -0500

    Add Link to rmdb logo to page Home

--- src/components/elements/Header.js
+++ src/components/elements/Header.js
@@ -2,11 +2,14 @@ import React from 'react';
 import rmdbLogo from '../images/reactMovie_logo.png';
 import tmdbLogo from '../images/tmdb_logo.svg';
 import {StyledHeader, StyledRMDBLogo, StyledTMDBLogo} from '../styles/StyledHeader';
+import {Link} from '@reach/router';
 
 const Header = () => (
     <StyledHeader>
         <div className="header-content">
-            <StyledRMDBLogo src={rmdbLogo} alt="rmdb-logo"/>
+            <Link to="/">
+                <StyledRMDBLogo src={rmdbLogo} alt="rmdb-logo"/>
+            </Link>
             <StyledTMDBLogo src={tmdbLogo} alt="tmdb-logo"/>
         </div>
     </StyledHeader>
